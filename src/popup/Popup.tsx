92cb563b5bcb7737150606f21e9651b6f0b1b41b
shenxin <botzgbj@gmail.com>
2023-04-02 23:06:11 +0800

    feat: add dependencies

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -8,7 +8,7 @@ function App() {
     <main>
       <h3>Popup Page!</h3>
 
-      <h6>v 0.0.0</h6>
+      <h6>v 0.0.1</h6>
 
       <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">
         Power by {crx}
